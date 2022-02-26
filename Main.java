6a2790bd950cd46b9e4fc9fedaf802ba889ac407
радон <96480522+RadonRebirth@users.noreply.github.com>
2022-02-26 09:16:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 package com.company;
+
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         Random rnd = new Random();
         int[] nums = new int[5];
+        System.out.println("Массив :");
         for(int i=0;i<nums.length;i++) {
             nums[i] = rnd.nextInt(20);
+            System.out.print(" " + nums[i]);
         }
+        System.out.println();
         int min = nums[0];
         double Sredn = 0;
-
         for(int i = 0; i<nums.length; i++){
             if(nums[i]< min){
                 min = nums[i];
@@ -27,6 +32,5 @@ public class Main {
         System.out.println("Среднее арифметическое значение "+ Sredn);
         System.out.println("Сумма мин. и сред. арифметического значения " + summa);
     }
-
 }
 
